5ce78d3a0ac460bdb35455f959447828ad443f94
Nizami Amirov <amirovnizami395@gmail.com>
2025-01-20 22:24:38 +0400

    HomeWork

--- Lesson2PracticeForm/Controllers/HomeController.cs
+++ Lesson2PracticeForm/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Lesson2PracticeForm.Entities;
+﻿using System;
+using Lesson2PracticeForm.Entities;
 using Lesson2PracticeForm.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -20,20 +21,35 @@ namespace Lesson2PracticeForm.Controllers
             };
             return View(user);
         }
+
         public IActionResult Index()
         {
-
             string filePath = Path.Combine(Directory.GetCurrentDirectory(),"Helpers","User.json");
 
             var jsonData = System.IO.File.ReadAllText(filePath);
             var user = JsonSerializer.Deserialize<List<User>>(jsonData);
-
             return View(user);
         }
-        public IActionResult Add()
-        {
 
+        public IActionResult WriteJson(User user)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "User.json");
+
+            List<User> users;
+            if (System.IO.File.Exists(filePath))
+            {
+                var jsonData = System.IO.File.ReadAllText(filePath);
+                users = JsonSerializer.Deserialize<List<User>>(jsonData) ?? new List<User>();
+            }
+            else
+            {
+                users = new List<User>();
+            }
+            users.Add(user);
+            string jsonString = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText(filePath, jsonString);
+
+            return RedirectToAction("Index");
         }
-        
     }
 }
